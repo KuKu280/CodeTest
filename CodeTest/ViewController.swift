a49ac89ddc397cbecbf928246647cdc305d32a77
Ku Ku Zan <kuku.zan@hiveinnovate.com>
2022-09-25 09:44:12 +0630

    project setup

---
+++ CodeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeTest
+//
+//  Created by Ku Ku Zan on 9/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
